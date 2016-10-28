811d61a2795d5f46277af1ec7799eac36ba204ec
Gilles Leblanc <gilles.leblanc@gmail.com>
2016-10-27 21:15:30 -0400

    First commit.

---
+++ Ajax2/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Ajax2.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Ajax2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new DepartmentsModel
+            {
+                Filtered = true,
+                Departments = GetDepartments(filtered: true),
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public PartialViewResult FilterDepartmentsAjax(DepartmentsModel model)
+        {
+            var updatedModel = new DepartmentsModel
+            {
+                Filtered = model.Filtered,
+                Departments = GetDepartments(filtered: model.Filtered),
+            };
+
+            return PartialView("DepartmentsTablePartial", updatedModel);
+        }
+
+        private IEnumerable<DepartmentModel> GetDepartments(bool filtered)
+        {
+            var departments = new List<DepartmentModel>
+            {
+                new DepartmentModel { Id = 1, Name = "Sales" },
+                new DepartmentModel { Id = 2, Name = "Accounting" },
+                new DepartmentModel { Id = 3, Name = "Marketing" },
+                new DepartmentModel { Id = 4, Name = "HR" },
+                new DepartmentModel { Id = 5, Name = "IT" },
+                new DepartmentModel { Id = 6, Name = "Customer Support" },
+            };
+
+            if (filtered)
+                return departments.Where(x => x.Id < 4);
+
+            return departments;
+        }
+    }
+}
---
+++ Ajax2/Models/DepartmentsModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Ajax2.Models
+{
+    public class DepartmentsModel
+    {
+        public bool Filtered { get; set; }
+        public IEnumerable<DepartmentModel> Departments { get; set; }
+
+        public DepartmentsModel()
+        {
+            Departments = new List<DepartmentModel>();
+        }
+    }
+}
